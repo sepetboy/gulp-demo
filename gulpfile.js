0cce34a9189dab6c478240d7bd67cf9295d2eeb2
hutingjin <179645132@qq.com>
2018-10-25 10:40:40 +0800

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
+// var gulp = require("gulp");
+// gulp.task("hello", function() {
+//   console.log("HelloWord");
+// })
 var gulp = require("gulp");
-gulp.task("hello", function() {
-  console.log("HelloWord");
+var sass = require("gulp-sass");
+
+gulp.task('sass', function () {
+  return gulp.src("app/scss/**/*.scss")
+          .pipe(sass()) // Converts Sass to CSS with gulp-sass
+          .pipe(gulp.dest("app/css"))
 })
